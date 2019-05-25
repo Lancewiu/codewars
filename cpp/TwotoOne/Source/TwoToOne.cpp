c48ddb6faf36952b9f7b62b5ef335b805d6688e7
Lawrence Niu <ranchwiu@gmail.com>
2019-05-25 11:25:49 -0400

    moved test directory to project folder

--- cpp/TwotoOne/Source/TwoToOne.cpp
+++ cpp/TwotoOne/Source/TwoToOne.cpp
@@ -1,4 +1,4 @@
-#include "..\Header\TwoToOne.h"
+#include "TwoToOne.h"
 #include <iterator>
 #include <set>
 
--- cpp/TwotoOne/TwoToOneUnitTests/TwoToOneUnitTests.cpp
+++ cpp/TwotoOne/TwoToOneUnitTests/TwoToOneUnitTests.cpp
@@ -1,18 +1,14 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "..\TwotoOne\Header\TwoToOne.h"
+#include "TwoToOne.h"
 
 #include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-void testequal(std::string ans, std::string sol) {
-	Assert::AreEqual(ans, sol);
-}
-
 void dotest(std::string a1, std::string a2, std::string expected)
 {
-	testequal(TwoToOne::longest(a1, a2), expected);
+	Assert::AreEqual(TwoToOne::longest(a1, a2), expected);
 }
 
 namespace TwoToOneUnitTests
